3b5ab74ddb45b6758b456c3776e24f06fd2b3210
Sergey <sergey198891@mail.ru>
2023-02-20 16:23:04 +0300

    mistakes correction

--- scripts/index.js
+++ scripts/index.js
@@ -4,47 +4,34 @@ let popUp = document.querySelector('.popup');
 
 let popupCloseButton = document.querySelector('.popup__close-button');
 
+let userNameElement = document.querySelector('.profile__title');
+
+let userJobElement = document.querySelector('.profile__subtitle');
+
+let formElement = document.querySelector('.popup__container-form');
+
+let nameInput = document.querySelector('.popup__input_type_name');
+
+let jobInput = document.querySelector('.popup__input_type_job');
+
 function openPopup () {
   popUp.classList.add('popup_opened');
+  nameInput.value = userNameElement.textContent;
+  jobInput.value = userJobElement.textContent;
 }
 
 function closePopup () {
   popUp.classList.remove('popup_opened');
 }
 
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    userNameElement.textContent = nameInput.value;
+    userJobElement.textContent = jobInput.value;
+    closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
 profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-
-let userName = 'Жак-Ив Кусто';
-
-let userJob = 'Исследователь океана';
-
-let userNameElement = document.querySelector('.profile__title');
-userNameElement.textContent = userName;
-
-let userJobElement = document.querySelector('.profile__subtitle');
-userJobElement.textContent = userJob;
-
-let popupInputName = document.querySelector('.popup__input_type_name');
-popupInputName.value = userName;
-
-let popupInputJob = document.querySelector('.popup__input_type_job');
-popupInputJob.value = userJob;
-
-let popupSaveButton = document.querySelector('.popup__container-button');
-
-popupSaveButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  userNameElement.textContent = popupInputName.value;
-  userJobElement.textContent = popupInputJob.value;
-  userName = popupInputName.value;
-  userJob = popupInputJob.value;
-  closePopup();
-})
-
-popupCloseButton.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popupInputName.value = userName;
-  popupInputJob.value = userJob;
-  closePopup();
-})
